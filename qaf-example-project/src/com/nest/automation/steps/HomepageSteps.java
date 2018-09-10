bc42b0588a093bef2cc8f311f216220dd993c1e7
dheerajkumar30029 <dheeraj.kumar@infostretch.com>
2018-09-10 18:41:53 +0530

    10Monday2018

---
+++ qaf-example-project/src/com/nest/automation/steps/HomepageSteps.java
@@ -0,0 +1,5 @@
+package com.nest.automation.steps;
+
+public class HomepageSteps {
+
+}
--- qaf-example-project/src/com/nest/automation/steps/Loginsteps.java
+++ qaf-example-project/src/com/nest/automation/steps/Loginsteps.java
@@ -1,19 +1,24 @@
 package com.nest.automation.steps;
 
+import org.testng.Assert;
+
+
 import com.qmetry.qaf.automation.core.ConfigurationManager;
 import com.qmetry.qaf.automation.step.CommonStep;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import com.qmetry.qaf.automation.ui.WebDriverTestBase;
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
 import com.qmetry.qaf.automation.ui.webdriver.QAFExtendedWebElement;
 import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
+import com.qmetry.qaf.automation.util.Reporter;
 import com.qmetry.qaf.automation.util.StringMatcher;
 
-public class Loginsteps {
-
+public class Loginsteps  {
+	
+	
 	@QAFTestStep(description="I open the nest application")
-	public void launchApp() {
+	public void iOpenNestApplication() {
 		new WebDriverTestBase().getDriver().get("/");
-		
 //		CommonStep.get("/");
 	}
 	@QAFTestStep(description="I login with {username} and password {password}")
@@ -21,27 +26,33 @@ public class Loginsteps {
 		CommonStep.sendKeys(username, "login.username.txtBx");
 		CommonStep.sendKeys(password, "login.password.txtBx");
 		CommonStep.click("login.submit.Btn");
+//		ConfigurationManager.getBundle().setProperty("loggedin.user",username);
 	}
 	
-	@QAFTestStep(description = "I should be on homepage {username}")
-	public void homePageTest(String username) {
-		QAFWebElement element = new QAFExtendedWebElement(String.format(ConfigurationManager.getBundle().getString("home.user.label"), username));
-		element.verifyText(StringMatcher.contains(username));
-//		CommonStep.verifyText("homepage.profile.name", "abc pqr");
-	
-//	@QAFTestStep(description="It should be on homepage")
-//	public void verifyLogin() {
-//		
-//		CommonStep.assertPresent("home.logo.img");
-//	}
-	
+	@QAFTestStep(description = "I should be on {name} homepage")
+	public void homePageTest(String name) {
+		ConfigurationManager.getBundle().setProperty("loggedin.user",name);
+		QAFWebElement element = new QAFExtendedWebElement(String.format(ConfigurationManager.getBundle().getString("home.user.label"), name));
+		element.verifyText(StringMatcher.contains(name));//Verify Username after Login
+//	CommonStep.verifyPresent("home.logo.img");
 }
 	@QAFTestStep(description="I Logout from APP")
 	public void logoutFromApp() {
-		QAFWebElement logoutWbElement=new QAFExtendedWebElement(ConfigurationManager.getBundle().getString("home.logout.btn"));
-		logoutWbElement.waitForVisible(200);
-		logoutWbElement.waitForPresent();
-		logoutWbElement.click();
+//		QAFWebElement loadIconElement=new QAFExtendedWebElement(ConfigurationManager.getBundle().getString("home.load.icon"));
+//		loadIconElement.waitForNotVisible();
+		Reporter.log(ConfigurationManager.getBundle().getString("loggedin.user")+" Has sucessufully Logged in");
+		CommonStep.waitForNotVisible("home.load.icon");
+//		logoutWbElement.waitForPresent();
+		CommonStep.click("home.logout.btn");
+		CommonStep.verifyPresent("login.form.Box");//Verify for Login Page 
+	}
+	@QAFTestStep(description="It should show ERROR {error_msg}")
+	public void verifyInvalidLogin(String expMsg)
+	{
+		String actMsg = CommonStep.getText("login.errorMsg.label");
+		Assert.assertEquals(actMsg, expMsg);
+		Reporter.logWithScreenShot("Error msg Displayed");
+//		import com.qmetry.qaf.automation.util.Reporter;
 	}
 	
 }
